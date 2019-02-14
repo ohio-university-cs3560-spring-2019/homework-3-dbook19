6984d99e7142ee7c9b9f0ddc31ff19d6766b11ce
Donovan Booker <46974574+dbook19@users.noreply.github.com>
2019-02-14 12:50:03 -0500

    Update hw_broken.cc

--- hw_broken.cc
+++ hw_broken.cc
@@ -4,21 +4,30 @@
 
 
 
-int deviation( int* a, int n )
+/*
+	This function displays the standard deviation of a set of elements in a array.
+	Parameter a specifies a pointer pointing to the memory address of the array elements.
+	Parameter n specifies the size of the array.
+	By using a mathmetical formula, the program reads the elements of the array and calculates
+	the standard deviation of those elements.
+*/
+double deviation( int* a, int n )
 {
-	int sum;
-	for(size_t i = 0; i <= v.size(); i++)
+	int sum = 0;
+	for(int i = 0; i <= n-1; i++)
 	{
-		sum += v[i];
+		sum += a[i];
 	} 
-	double mean = sum /= v.size();
-	double stddev = 0;
-	for(size_t i = 0; i <= v.size() -1; i++)
+	double mean = sum /= (n-1);
+	double stddev = 0.0;
+	for(int i = 0; i <= n-2; i++)
 	{
-		double stddev = stddev + (v[i] - mean) * (v[i] - mean); 
+		double stddev = stddev + pow( a[i] - mean, 2); 
 	}
-	stddev /= v.size();
-	if( stddev = 0)
+	stddev /= (n-1);
+	if( stddev == 0.0)
+	{
 		std::cout << "Sigma is zero." << std::endl;
+	}
 	return sqrt(stddev);
 }
